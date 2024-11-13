2dbb29964967f2101d9a2d25fe9aa0a2d356da41
Udeh Tochukwu Tobias <jaxj9449@gmail.com>
2024-11-14 00:50:40 +0100

    changes in npm

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import SkillsSection from "@/components/sections/skills-section/SkillsSection";
 import AboutSection from "../components/sections/about-section/AboutSection"
 import ContactSection from "../components/sections/contact-section/ContactSection";
 import FeaturedProjects from "../components/sections/featured-projects/FeaturedProjects";
 import ProfileSection from "../components/sections/profile-section/ProfileSection"
-import SkillsSection from "../components/sections/skills-section/SkillsSection"
 
 
 
